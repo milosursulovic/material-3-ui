83a27119265968fa5e15241bd559c907da1527fe
Misa <milosursulovic2696@gmail.com>
2023-01-26 16:25:24 +0100

    Init project

---
+++ app/src/main/java/com/example/material3ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.material3ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.material3ui.ui.theme.Material3UITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Material3UITheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
